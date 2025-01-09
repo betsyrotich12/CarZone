bdd346d72225c26db84fd48e5863c77b8b9d6b03
Betsy Rotich <BRotich@ccbagroup.com>
2025-01-09 13:42:44 +0300

    Added templates

--- Pages/views.py
+++ Pages/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'Pages/home.html')
+
+def about(request):
+    return render(request, 'Pages/about.html')
+
+def services(request):
+    return render(request, 'Pages/services.html')
+
+def contact(request):
+    return render(request, 'Pages/contact.html')
--- carzone/settings.py
+++ carzone/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Pages',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
